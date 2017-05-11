c552e6c62d8ec256883b405f332185cce8ae6b01
Daniel Makover <dmakover@gmail.com>
2017-05-11 11:51:21 -0400

    Refactor: Routing

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,7 +1,31 @@
-module.exports = function(router){
+module.exports = function(app , dir){
 
-  router.get('/', function(req, res){
- 
+  //ROUTES
+  app.get('/', function(req, res){
+    console.log(dir);
+    res.render(dir + '/public/views/index.ejs');
+  });
+
+  app.get('/games', function(req, res){
+    var game_id = req.param('game');
+    res.render(dir + '/public/views/game.ejs',  {game_id});
+  });
+
+  app.get('/verticalmario', function(req, res){
+    res.render(dir + '/public/views/verticalmario.ejs');
+  });
+
+  app.get('/asteroids', function(req, res){
+    var game_id = req.param('game');
+    res.render(dir +'/public/views/asteroids.ejs');
+  });
+
+  app.get('/about', function(req, res){
+    res.render(dir +'/public/views/about.ejs');
+  });
+  app.get('/contact', function(req, res){
+    var game_id = req.param('game');
+    res.render(dir + '/public/views/contact.ejs',  {game_id});
   });
 
 }
--- server.js
+++ server.js
@@ -17,32 +17,7 @@ app.use(express.static(__dirname + '/public'));
 
 app.set('view engine', 'ejs');
 
-//ROUTES
-app.get('/', function(req, res){
-  res.render(__dirname + '/public/views/index.ejs');
-});
-
-app.get('/games', function(req, res){
-  var game_id = req.param('game');
-  res.render(__dirname + '/public/views/game.ejs',  {game_id});
-});
-
-app.get('/verticalmario', function(req, res){
-  res.render(__dirname + '/public/views/verticalmario.ejs');
-});
-
-app.get('/asteroids', function(req, res){
-  var game_id = req.param('game');
-  res.render(__dirname + '/public/views/asteroids.ejs');
-});
-
-app.get('/about', function(req, res){
-  res.render(__dirname + '/public/views/about.ejs');
-});
-app.get('/contact', function(req, res){
-  var game_id = req.param('game');
-  res.render(__dirname + '/public/views/contact.ejs',  {game_id});
-});
+require('./app/routes/api.js')(app, __dirname);
 
 
 app.listen(port, function() {
